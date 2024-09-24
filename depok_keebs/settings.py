a4fb3c20fbbe0258858f2d51c1519cfeeaefb042
RayhanSP <ray.han2005.rh@gmail.com>
2024-09-25 02:44:08 +0700

    Tugas 4 done

--- depok_keebs/settings.py
+++ depok_keebs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2+spfgs80h$7t#eg*z)a8_65_9mt5&uorc+ojd5qayli)v03ug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rayhan-syahdira-depokkeebs.pbp.cs.ui.ac.id"]
 
